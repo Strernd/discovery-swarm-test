e938f7b06db056f24d9bc3ecffcdb2fcb2d16fb4
Bernd Strehl <bernd.strehl@gmail.com>
2019-08-22 13:17:56 +0200

    add fixes

--- index.js
+++ index.js
@@ -1,72 +1,55 @@
 const { prompt } = require('enquirer');
-const discovery = require('dns-discovery')
 const server = ['195.63.103.144', '81.169.241.28'];
-const disc = discovery({
-    server
-})
 
 const swarm = require('discovery-swarm');
 
-const sw = swarm({ dht: false, dns: disc })
-const port = Math.round(Math.random() * 9000) + 999;
+const sw = swarm({
+    dht: false, dns: {
+        server
+    }
+})
+const port = Math.round(Math.random() * 100) + 6666;
+console.log("Running on port " + port)
+sw.listen(port)
+sw.join('tixl') // can be any id/name/hash
+console.log('Joined channel "tixl"');
 
-const main = async () => {
+const connections = new Map();
 
+const handleIncoming = (id, data) => {
+    console.log(id, data);
+}
+
+sw.on('connection', function (connection, info) {
+    const id = info.id.toString('hex');
+    connections.set(id, connection);
+    console.log('Added connection with id ', id)
+    connection.on('data', chunk => handleIncoming(id, JSON.parse(chunk.toString())))
+})
+
+sw.on('connection-closed', (connection, info) => {
+    if (!info || !info.id) return;
+    const id = info.id.toString('hex');
+    console.log('Removed connection with id ', id)
+    connection;
+    connections.delete(id);
+})
+const input = async () => {
     try {
 
         const response = await prompt({
             type: 'input',
-            name: 'channel',
-            message: 'Which channel:'
+            name: 'message',
+            message: 'Message:'
         });
-
-        console.log({ port });
-        sw.listen(port)
-        console.log('Joined channel', response.channel)
-        sw.join(response.channel) // can be any id/name/hash
-
-        const connections = new Map();
-
-        const handleIncoming = (id, data) => {
-            console.log(id, data);
-        }
-
-        sw.on('connection', function (connection, info) {
-            const id = info.id.toString('hex');
-            connections.set(id, connection);
-            console.log('Added connection with id ', id)
-            connection.on('data', chunk => handleIncoming(id, JSON.parse(chunk.toString())))
-        })
-
-        sw.on('connection-closed', (connection, info) => {
-            if (!info || !info.id) return;
-            const id = info.id.toString('hex');
-            console.log('Removed connection with id ', id)
-            connection;
-            connections.delete(id);
-        })
-        const input = async () => {
-            try {
-
-                const response = await prompt({
-                    type: 'input',
-                    name: 'message',
-                    message: 'Message:'
-                });
-                for (const con of connections.values()) {
-                    con.write(Buffer.from(JSON.stringify({ message: response.message })))
-                }
-                input();
-            } catch (error) {
-                console.error(error);
-                process.exit();
-            }
-
+        for (const con of connections.values()) {
+            con.write(Buffer.from(JSON.stringify({ message: response.message })))
         }
         input();
     } catch (error) {
         console.error(error);
         process.exit();
     }
+
 }
-main();
+input();
